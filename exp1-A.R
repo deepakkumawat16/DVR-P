2ea4f27c0b767c2109c22ecf1f6c7ee53abf7576
Deepak kumawat <dk4870336@gmail.com>
2024-11-28 21:29:11 +0530

    Create exp1-A.R

---
+++ exp1-A.R
@@ -0,0 +1,21 @@
+# Using the <- operator
+x <- 10                # Numeric variable
+name <- "Alice"        # Character variable
+is_valid <- TRUE       # Logical variable
+values <- c(1, 2, 3, 4) # Vector of numeric values
+
+# Using the = operator
+age = 25                # Numeric variable
+city = "New York"       # Character variable
+has_pets = FALSE        # Logical variable
+grades = c(90, 85, 88)  # Vector of numeric values
+
+# Print the variables to check their values
+print(x)
+print(name)
+print(is_valid)
+print(values)
+print(age)
+print(city)
+print(has_pets)
+print(grades)
